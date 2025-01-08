ef91c4c972d3473ef7780c7ba2e7e97af50de92b
Alao Abiodun <abiodundev@gmail.com>
2025-01-08 14:51:25 +0100

    chore: update the bootstrap for express app

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // index route
 app.get('/cachie', async (req: Request, res: Response, next: NextFunction) => {
-    // return res.status(200).json({
-    //     message: 'Welcome to Cachie 🚀'
-    // })
     console.log(res);
 })
 
