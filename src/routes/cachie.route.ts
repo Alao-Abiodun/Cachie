99af4dda5a803184716c83f3a2e41a14ff9265b9
Alao Abiodun <abiodundev@gmail.com>
2025-01-09 12:30:31 +0100

    chore: add rate limiting using in memory case

--- src/routes/cachie.route.ts
+++ src/routes/cachie.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as cachieController from '../controllers/cachie.controller';
+import { rateLimiterMiddleware } from '../utils/lib/rateLimiter';
 
 export default (router: Router) => {
     router.post('/search', cachieController.searchClientTrackingInfo);
