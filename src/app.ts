9fdd98303157ed367cce0400ca565c19794655f7
Alao Abiodun <abiodundev@gmail.com>
2025-01-09 06:35:52 +0100

    chore: implement logging for monitoring and tracing

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ const app: Application = express();
 
 //get routes
 import routes from './routes/index.route';
+import { StatusCodes } from 'http-status-codes';
 
 // setup middleware
 app.use(express.json());
@@ -20,13 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/cachie/v1', routes);
 
 // index route
-app.get('/cachie', async (req: Request, res: Response, next: NextFunction) => {
-    console.log(res);
+app.get('/cachie', async (req: Request, res: any, next: NextFunction) => {
+    return res.status(200).json({
+        message: "Welcome to Cachie 🚀"
+    })
 })
 
 // handle 404 routes
-app.get('*', async (req: Request, res: Response, next: NextFunction) => {
-    console.log(res);
+app.get('*', async (req: Request, res: any, next: NextFunction) => {
+    return res.status(StatusCodes.NOT_FOUND).json({
+        message: `Resource ${req.originalUrl} does not exist`
+    })
 })
 
 export default app;
