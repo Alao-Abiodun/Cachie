13c61c64438b6d9e1ab5651e91dd9eb1c71bfc2a
Alao Abiodun <abiodundev@gmail.com>
2025-01-10 20:24:05 +0100

    chore: add ratelimit as middle to the search endpoint

--- src/routes/cachie.route.ts
+++ src/routes/cachie.route.ts
@@ -3,6 +3,25 @@ import * as cachieController from '../controllers/cachie.controller';
 import { rateLimiterMiddleware } from '../utils/lib/rateLimiter';
 
 export default (router: Router) => {
-    router.post('/search', cachieController.searchClientTrackingInfo);
+    router.post('/search', rateLimiterMiddleware, cachieController.searchClientTrackingInfo);
     router.get('/analyze', cachieController.analyseSearchData);
 }
+
+//search document
+/**
+ * {
+ *   id: Object.ID
+ *   search_query: String,
+ *   client_id: String,
+ *   session_id: String,
+ * }
+ */
+
+// analyze document
+/**
+ * {
+ *   exact_matches: Number,
+ *  fuzzy_matches: Number,
+ *  search: ref(search document)
+ * }
+ */
