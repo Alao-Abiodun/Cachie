f7f8142abd4d27dedb0f64fe67517c076b4d5445
Alao Abiodun <abiodundev@gmail.com>
2025-01-10 20:26:05 +0100

    chore: remove comment code

--- src/routes/cachie.route.ts
+++ src/routes/cachie.route.ts
@@ -6,22 +6,3 @@ export default (router: Router) => {
     router.post('/search', rateLimiterMiddleware, cachieController.searchClientTrackingInfo);
     router.get('/analyze', cachieController.analyseSearchData);
 }
-
-//search document
-/**
- * {
- *   id: Object.ID
- *   search_query: String,
- *   client_id: String,
- *   session_id: String,
- * }
- */
-
-// analyze document
-/**
- * {
- *   exact_matches: Number,
- *  fuzzy_matches: Number,
- *  search: ref(search document)
- * }
- */
